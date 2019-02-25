14e2338cf9e73cf8188413b9a2530209472c3b68
Gloomy <gloomyneter@gmail.com>
2019-02-25 19:03:51 +0800

    init

---
+++ GPELoader/BaseLoader.h
@@ -0,0 +1,12 @@
+#pragma once
+#pragma once
+
+#include <minwindef.h>
+
+class BaseLoader {
+public:
+	virtual BOOL init() = 0;
+	virtual BOOL load() = 0;
+	virtual BOOL unload() = 0;
+	virtual BOOL outputErrorMsg() = 0;
+};
